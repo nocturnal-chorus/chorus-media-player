792a528127ad1488c021ca9811a80e69434ca2e0
tober_r1 <2389704501@qq.com>
2023-09-05 23:03:28 +0800

    core: import just_audio player library

--- player/linux/flutter/generated_plugin_registrant.cc
+++ player/linux/flutter/generated_plugin_registrant.cc
@@ -10,17 +10,14 @@
 #include <system_theme/system_theme_plugin.h>
 #include <window_manager/window_manager_plugin.h>
 
-void fl_register_plugins(FlPluginRegistry *registry) {
-    g_autoptr(FlPluginRegistrar)
-    screen_retriever_registrar =
-            fl_plugin_registry_get_registrar_for_plugin(registry, "ScreenRetrieverPlugin");
-    screen_retriever_plugin_register_with_registrar(screen_retriever_registrar);
-    g_autoptr(FlPluginRegistrar)
-    system_theme_registrar =
-            fl_plugin_registry_get_registrar_for_plugin(registry, "SystemThemePlugin");
-    system_theme_plugin_register_with_registrar(system_theme_registrar);
-    g_autoptr(FlPluginRegistrar)
-    window_manager_registrar =
-            fl_plugin_registry_get_registrar_for_plugin(registry, "WindowManagerPlugin");
-    window_manager_plugin_register_with_registrar(window_manager_registrar);
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) screen_retriever_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ScreenRetrieverPlugin");
+  screen_retriever_plugin_register_with_registrar(screen_retriever_registrar);
+  g_autoptr(FlPluginRegistrar) system_theme_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "SystemThemePlugin");
+  system_theme_plugin_register_with_registrar(system_theme_registrar);
+  g_autoptr(FlPluginRegistrar) window_manager_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "WindowManagerPlugin");
+  window_manager_plugin_register_with_registrar(window_manager_registrar);
 }
